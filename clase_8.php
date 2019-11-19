ebeeb02f3abe3f7c6a1f874da093f1f51026b0b6
Jorge La Massa <lamassa1987@gmail.com>
2019-11-19 16:31:05 +0100

    commit 'clase8'

--- clase_8.php
+++ clase_8.php
@@ -1,3 +1,38 @@
 <?php
 
+    abstract class molde{
+        //atributos
+        
+        //metodos
+        abstract public function ingresar_nombre($nombre);
+
+        abstract public function obtener_nombre();
+    }
+    
+    class persona extends molde
+    {
+       //atributos
+        private $mensaje = "clase 8 de php.";
+        private $nombre;
+       //metodos
+        public function mostrar()
+        {
+            echo $this -> mensaje;
+        }
+
+        public function ingresar_nombre($nombre)
+        {
+           $this -> nombre = $nombre;  
+        }
+
+        public function obtener_nombre()
+        {
+           echo " el nombre es del alumno es: " .$this -> nombre; 
+        }
+    }
+
+    $obj = new persona();
+    $obj -> mostrar();
+    $obj -> ingresar_nombre("jorge");
+    $obj -> obtener_nombre();
 ?>
